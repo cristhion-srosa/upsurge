20c9ede17d6294fd9827e52a6172e1f36561396e
cristhion rosa <silvacristhion@gmail.com>
2026-06-25 17:46:34 -0300

    chore: configure elysia

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("Hello via Bun!");
+import { Elysia } from "elysia";
+
+const { PORT } = process.env;
+
+const app = new Elysia()
+	.get("/health", () => ({ status: "ok" }))
+	.listen(PORT ?? 3000);
+
+console.log(
+	`Server running at http://${app.server?.hostname}:${app.server?.port}`,
+);
